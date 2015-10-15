e2a79a14342e663f8f59c15b3f49c22de0354c4f
raymondwu1 <raymond_wu1995@hotmail.com>
2015-10-15 12:09:05 -0400

    Create user.java

---
+++ user.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class user {
+	
+		protected String taste;
+		protected List<document> likedDocs;
+		protected List<user> followedBy;
+		protected List<user> following;
+		
+		public user(String taste){
+			this.taste = taste;
+			followedBy = new ArrayList<user>();
+			following = new ArrayList<user>();
+			likedDocs = new ArrayList<document>();
+		}
+
+		public void search() {
+			
+			
+		}
+
+		public int payoff() {
+			return 0;
+		}
+		
+		public String getTaste() {
+			return taste;
+		}
+		
+		public int numberOfFollows(){
+			return followedBy.size();
+		}
+
+	}
