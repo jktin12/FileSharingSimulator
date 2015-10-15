9c01e142819f2e3e0c5035abe1f6de70193c2941
raymondwu1 <raymond_wu1995@hotmail.com>
2015-10-15 15:01:44 -0400

    Still needs some work

--- user.java
+++ user.java
@@ -12,6 +12,7 @@ public class user {
 		protected List<document> likedDocs;
 		protected List<user> followedBy;
 		protected List<user> following;
+		protected List<document> top5;
 		
 		public user(String taste){
 			this.taste = taste;
@@ -20,14 +21,55 @@ public class user {
 			likedDocs = new ArrayList<document>();
 		}
 
+		//Simple implementation of search using only the
+		//popularity of a document
 		public void search(simulation s) {
+			//Get the first document in the list
+			List<document> temp = new ArrayList<document>();
+			temp = s.getAllDocuments();
+			document currentBest;
 			
-			
+			while (top5.size()<5) {	
+				currentBest = temp.get(0);
+				temp.remove(0);
+				for (document d:temp) {
+					if (currentBest.calculatePop() < d.calculatePop()) {
+						currentBest = d;
+					}
+				}
+				//Add the highest popularity document to the top3 list
+				top5.add(currentBest);
+				temp.remove(currentBest);
+			}
 		}
 
-		
+		//Calculates the performance for the search
 		public int payoff() {
-			return 0;
+			int a=0;
+			for (document d:top5){
+				if ((d.getTag()==this.taste) && !(likedDocs.contains(d)))
+					a=a+1;
+			}
+			return a;
+		}
+		
+		public void followOrLike(){
+		
+			//Iterates through each document in the top 5
+			for (document d:top5){
+				//If the user has the same taste as the document
+				//and is not currently liking it
+				//add the document to the users liked list
+				if ((d.getTag()==this.taste) && !(likedDocs.contains(d)))
+					this.addLikedDoc(d);
+				
+				//Follow the users that like the document
+				//if the user is not currently following them
+				for (user u:d.getUserLiked()){
+					if (!(following.contains(u)))
+						following.add(u);
+				}
+			}
 		}
 		
 		//Adds a follower
@@ -49,7 +91,7 @@ public class user {
 			return taste;
 		}
 		
-		public int numberOfFollows(){
+		public int numberOfFollowers(){
 			return followedBy.size();
 		}
 
