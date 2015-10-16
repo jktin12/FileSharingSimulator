cf20c21e5e9d7bf59925e228c276803115e6f9db
marctebo <marctebo@gmail.com>
2015-10-16 19:48:47 -0400

    Added Network Class
    Created outline for network class to match Network class diagram in UML.

--- SYSC_3110_Project/src/nullSquad/network/Network.java
+++ SYSC_3110_Project/src/nullSquad/network/Network.java
@@ -1,6 +1,56 @@
+/**
+ * Title: SYSC 3110 Project
+ * Team: nullSquad
+ * To be implemented by Justin
+ */
+
 package nullSquad.network;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Network
 {
+	private List<User> users;
+	private List<Document> allDocuments;
+	
+	public Network()
+	{
+		users = new ArrayList<>();
+		allDocuments = new ArrayList<>();
+	}
+	
+	public boolean registerUser(User user)
+	{
+		return false;
+	}
+	public boolean deactivateUser(User user)
+	{
+		return false;
+	}
+	public int calculatePayoff(List<Document> documents)
+	{
+		return 0;
+	}
+	public List<Document> search(int topK)
+	{
+		return allDocuments;
+	}
+	public boolean addDocument(Document doc)
+	{
+		return false;
+	}
+	public boolean removeDocument(Document doc)
+	{
+		return false;
+	}
+	public List<Document> getAllDocuments()
+	{
+		return allDocuments;
+	}
+	public List<User> getUsers()
+	{
+		return users;
+	}
 
 }
