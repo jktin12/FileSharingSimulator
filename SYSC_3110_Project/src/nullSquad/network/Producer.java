c16a2b8e0969ea82766496d8f9d9660d7bbb4703
MikeVezina <vezina95@bell.net>
2015-10-17 19:31:53 -0400

    Updated Document.java
    Updated the following methods:
    - Changed getDate() to getDateUploaded()
    - Changed allLikers() to getUserLikes()
    - Changed getDocID() to getDocumentID()
    - Added getDocumentName()

--- SYSC_3110_Project/src/nullSquad/network/Producer.java
+++ SYSC_3110_Project/src/nullSquad/network/Producer.java
@@ -1,6 +1,31 @@
 package nullSquad.network;
 
+import java.util.*;
+
 public class Producer extends User
 {
+	private List<Document>	docsProduced;
+
+	public Producer(int userID, String taste)
+	{
+		super(userID, taste);
+		this.docsProduced = new ArrayList<>();
+	}
+
+	@Override
+	public void act(Network net)
+	{
+		// We want to iterate through all documents and like any that match our taste
+		for (Document d : net.getAllDocuments())
+		{
+			// Like a document if it matches our taste or it is a document that we produced
+			if (d.getTag() == super.taste || d.getProducer().equals(this))
+			{
+				d.likeDocument(this);
+			}
+		}
+		
+		
+	}
 
 }
