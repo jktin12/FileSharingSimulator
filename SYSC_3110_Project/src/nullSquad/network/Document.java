8a87e7049c4ce3f9cb490ddbedbca7d5c77ad3b5
MikeVezina <vezina95@bell.net>
2015-10-17 19:32:43 -0400

    Revert "Updated Document.java"
    This reverts commit c16a2b8e0969ea82766496d8f9d9660d7bbb4703.

--- SYSC_3110_Project/src/nullSquad/network/Document.java
+++ SYSC_3110_Project/src/nullSquad/network/Document.java
@@ -13,21 +13,20 @@ import java.util.List;
 
 public class Document
 {
-	private String		name;
-	private String		tag;
-	private int			docID;
-	private List<User>	likedBy;
-	private Producer	producer;
-	private Date		dateUploaded;
-
+	private String name;
+	private String tag;
+	private int docID;
+	private List<User> likedBy;
+	private Producer producer;
+	private Date dateUploaded;
+	
 	/**
 	 * @author Marc Tebo
-	 *         Creates a Document with a name, tag and producer who created the
-	 *         document
+	 * Creates a Document with a name, tag and producer who created the document
 	 * 
-	 * @param name Name of the document
-	 * @param tag Search tag that will be used to find this document
-	 * @param producer Creator of the document
+	 * @param name	Name of the document
+	 * @param tag	Search tag that will be used to find this document
+	 * @param producer	Creator of the document
 	 * 
 	 */
 	public Document(String name, String tag, Producer producer, int docID)
@@ -39,11 +38,10 @@ public class Document
 		likedBy = new ArrayList<>();
 		dateUploaded = Calendar.getInstance().getTime();
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 *         User who "likes" this document will be added to the list of
-	 *         people who like this document
+	 * User who "likes" this document will be added to the list of people who like this document
 	 * 
 	 * @param user User who "likes" the document
 	 * @return whether or not the document was "liked"
@@ -54,107 +52,98 @@ public class Document
 		{
 			return false;
 		}
-
+		
 		likedBy.add(user);
 		return true;
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 *         User who wants to "unlike" this document will be removed from the
-	 *         list of people who liked it
-	 * @param user User who "unlikes" the document
-	 * @return whether or not the doucument was "unliked"
+	 * User who wants to "unlike" this document will be removed from the list of people who liked it
+	 * @param user	User who "unlikes" the document
+	 * @return	whether or not the doucument was "unliked"
 	 */
 	public boolean unlikeDocument(User user)
 	{
-		if (!likedBy.contains(user))
+		if(!likedBy.contains(user))
 		{
 			return false;
 		}
 		likedBy.remove(user);
 		return true;
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 *         Gets all users who have "liked" this document
-	 * @return List of users who "liked" the document
+	 * Gets all users who have "liked" this document
+	 * @return	List of users who "liked" the document
 	 */
-	public List<User> getUserLikes()
+	public List<User> allLikers()
 	{
 		return likedBy;
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 *         Returns string representation of the document
+	 * Returns string representation of the document
 	 */
 	public String toString()
-	{
+	{		
 		return ("Name: " + this.name + "\nTag: " + this.tag + "\nProducer:  " + this.producer + "\nDate Uploaded: " + this.dateUploaded);
-
+		
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 *         Compares if two documents are the same
+	 * Compares if two documents are the same
 	 * @returns true if documents have the same unique bookID
 	 */
 	public boolean equals(Object o)
 	{
-		// If the two object references are equal, return true
-		if (this == o)
-			return true;
+			// If the two object references are equal, return true
+			if (this == o)
+				return true;
+			
+			
+			// If the object is not an instance of Document, return false
+			if (!(o instanceof Document))
+				return false;
 
-		// If the object is not an instance of Document, return false
-		if (!(o instanceof Document))
-			return false;
-
-		// Returns whether the two document IDs are the same
-		return (((Document) o).docID == this.docID);
+			// Returns whether the two document IDs are the same
+			return (((Document) o).docID == this.docID);
 
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 * @return Document Tag
+	 * @return	Document Tag
 	 */
 	public String getTag()
 	{
 		return tag;
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 * @return Document ID
+	 * @return	Document ID
 	 */
-	public int getDocumentID()
+	public int getDocID()
 	{
 		return docID;
 	}
-
+	
 	/**
 	 * @author Marc Tebo
-	 * @return Document Upload Date
+	 * @return	Document Upload Date
 	 */
-	public Date getDateUploaded()
+	public Date getDate()
 	{
 		return dateUploaded;
 	}
-
-	/**
-	 * @author MVezina
-	 * @return Document Name
-	 */
-	public String getDocumentName()
-	{
-		return name;
-	}
-
+	
 	/**
 	 * @author Justin Krol
-	 * @return Document Producer
+	 * @return	Document Producer
 	 */
 	public Producer getProducer()
 	{
