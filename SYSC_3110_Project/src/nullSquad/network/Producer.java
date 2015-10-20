9aa4ce89741fbfa7cf6ba92c47682766779a09c7
MikeVezina <vezina95@bell.net>
2015-10-19 22:29:20 -0400

    Added Producer Functionality
    - Extends User
    - Implements default ProducerPayoffStrategy
    - Added act method for simulator
    - Added getDocumentsProduced()
    - Override addFollower (from User)
    - Override toString()
    - Override equals(..)

--- SYSC_3110_Project/src/nullSquad/network/Producer.java
+++ SYSC_3110_Project/src/nullSquad/network/Producer.java
@@ -1,6 +1,184 @@
+/**
+ * Title: SYSC 3110 Project
+ * 
+ * @author MVezina
+ *         Student Number: 100934579
+ *         Team: noSquad
+ */
+
 package nullSquad.network;
 
-public class Producer extends User
+import java.util.*;
+
+import nullSquad.document.*;
+import nullSquad.strategies.ProducerPayoffStrategy;
+
+public class Producer extends User implements ProducerPayoffStrategy,
+		DocumentLikeListener
 {
+	// The list of documents that the producer has produced
+	private List<Document>			docsProduced;
+
+	// The Producer Payoff Strategy to be used
+	private ProducerPayoffStrategy	payoffStrategy;
+
+	public Producer(ProducerPayoffStrategy payoffStrat, String taste)
+	{
+		this(taste);
+
+		// Set the payoff strategy if it is not null
+		if (payoffStrat != null)
+			this.payoffStrategy = payoffStrat;
+	}
+
+	/**
+	 * Default Constructor.
+	 * Creates a producer with the specified userID and taste
+	 * 
+	 * @param userID the userID of the new user
+	 * @param taste the taste of the user
+	 */
+	public Producer(String taste)
+	{
+		super(taste);
+		this.docsProduced = new ArrayList<Document>();
+
+		// Sets the default payoff strategy to the interface implementation in
+		// this class
+		this.payoffStrategy = this;
+	}
+
+	/**
+	 * The Producer Override of addFollower(..):
+	 * - Requires re-calculation of Payoff after adding a follower
+	 */
+	@Override
+	public boolean addFollower(User user)
+	{
+		boolean res = super.addFollower(user);
+		System.out.println("A User has followed you! Your updated payoff is: " + calculatePayoff());
+		return res;
+	};
+
+	/**
+	 * The Producer Acts By:
+	 * - Creates a new document (has same taste as Producer)
+	 * - Likes newly created document
+	 * - Searches network for top K documents
+	 * - Likes documents with own taste
+	 */
+	@Override
+	public void act(Network net)
+	{
+		// Call the superclass act first
+		super.act(net);
+
+		// Create a new document
+		Document newDoc = new Document("Document (" + Calendar.getInstance().getTime() + ")", this.taste, this);
+
+		// The document is now added to the network
+		net.addDocument(newDoc);
+
+		// Like your own document
+		this.likeDocument(newDoc);
+
+		// Search the network for top 10 documents
+		for (Document d : net.search(10))
+		{
+			// Like all documents with the same taste as this User
+			if (d.getTag().equals(taste))
+			{
+				this.likeDocument(d);
+
+				// Follow all users who like this document
+				for (User u : d.getUserLikes())
+				{
+					this.followUser(u);
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Calls the set Producer Payoff Strategy
+	 * 
+	 * @return The payoff of the producer using the selected strategy
+	 * @author MVezina
+	 */
+	public int calculatePayoff()
+	{
+		return payoffStrategy.producerPayoffStrategy(this);
+	}
+
+	/**
+	 * The Default Producer payoff method uses followers/likes to calculate
+	 * payoff.
+	 * One point is given for every person who likes the producer's documents
+	 * Two points are given for every follower the producer has
+	 * 
+	 * @author MVezina
+	 */
+	@Override
+	public int producerPayoffStrategy(Producer prod)
+	{
+		// Two points for every follower the producer has
+		int totalPayoff = prod.getFollowers().size() * 2;
+
+		// One point for every like the producer has
+		for (Document d : prod.getDocumentsProduced())
+		{
+			totalPayoff += d.getUserLikes().size();
+		}
+
+		return totalPayoff;
+	}
+
+	/**
+	 * @return The List of Documents Produced by the producer
+	 * @author MVezina
+	 */
+	public List<Document> getDocumentsProduced()
+	{
+		return this.docsProduced;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "User Type: Producer (Payoff: " + calculatePayoff() + ")\n" + super.toString();
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		// Call the user equals method
+		if (!super.equals(o))
+			return false;
+
+		// Check if the object is an instance of Producer
+		if (!(o instanceof Producer))
+			return false;
+
+		// Cast to producer
+		Producer p = (Producer) o;
+
+		// Perform comparisons
+		return (this.docsProduced.size() == p.docsProduced.size());
+
+	}
+
+	/**
+	 * DocumentLiked Event Handler
+	 */
+	@Override
+	public void DocumentLiked(DocumentLikeEvent docLikeEvent)
+	{
+		if(docLikeEvent.getSource().getProducer().equals(this))
+		{
+			// Calculate and print the payoff
+			System.out.println("Your Document has been liked! Current Payoff: " + calculatePayoff());
+		}
+	}
 
 }
