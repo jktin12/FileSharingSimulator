afb40fb8ad9e919b14c08fb2d916ba81f1f89aca
MikeVezina <vezina95@bell.net>
2015-10-20 13:31:16 -0400

    Changed Simulator Class
    - Changed Simulator to get commands from user

--- SYSC_3110_Project/src/nullSquad/simulator/Simulator.java
+++ SYSC_3110_Project/src/nullSquad/simulator/Simulator.java
@@ -1,6 +1,6 @@
 /**
  * Title: SYSC 3110 Project
- * Created By: Raymond Wu			
+ * Created By: Raymond Wu
  * Student Number: 100938326
  * Team: nullSquad
  */
@@ -12,24 +12,29 @@ import java.util.*;
 
 public class Simulator
 {
-	private Network network;
-	private Random randomNumber;
-	private enum tags { MUSIC, BOOKS, GAMING, SCHOOL, SPORTS, PROGRAMMING }
-	private List<String> allTastesAndTags;
-	
+	private Network	network;
+	private Random	randomNumber;
+
+	private enum tags
+	{
+		MUSIC, BOOKS, GAMING, SCHOOL, SPORTS, PROGRAMMING
+	}
+
+	private List<String>	allTastesAndTags;
+
 	/**
 	 * Creates a simulator with specified network
 	 * 
 	 * @param network Network of all the users/documents
 	 * @author Raymond Wu
 	 */
-	public Simulator (Network network) 
+	public Simulator(Network network)
 	{
 		this.network = network;
 		randomNumber = new Random();
 		allTastesAndTags = new ArrayList<String>();
 	}
-	
+
 	/**
 	 * Adds the specified number of consumers into the network
 	 *
@@ -37,16 +42,16 @@ public class Simulator
 	 * @param numberOfConsumers Number of consumers in the network
 	 * @author Raymond Wu
 	 */
-	private void createConsumers(int numberOfConsumers) 
+	private void createConsumers(int numberOfConsumers)
 	{
 		int randomTaste = randomNumber.nextInt(allTastesAndTags.size());
-		for (int x=0; x<numberOfConsumers;x++) 
-		{	
-			User user = new Consumer("Consumer"+x,allTastesAndTags.get(randomTaste));
+		for (int x = 0; x < numberOfConsumers; x++)
+		{
+			User user = new Consumer("Consumer" + x, allTastesAndTags.get(randomTaste));
 			user.registerUser(network);
 		}
 	}
-	
+
 	/**
 	 * Adds the specified number of producers into the network
 	 * 
@@ -57,13 +62,13 @@ public class Simulator
 	private void createProducers(int numberOfProducers)
 	{
 		int randomTaste = randomNumber.nextInt(allTastesAndTags.size());
-		for (int x=0; x<numberOfProducers;x++) 
+		for (int x = 0; x < numberOfProducers; x++)
 		{
-			User user = new Producer("Producer"+x,allTastesAndTags.get(randomTaste));
+			User user = new Producer("Producer" + x, allTastesAndTags.get(randomTaste));
 			user.registerUser(network);
 		}
 	}
-	
+
 	/**
 	 * Sets number of random tags in simulation based on given parameter
 	 * 
@@ -72,94 +77,126 @@ public class Simulator
 	 */
 	private void setTastesAndTags(int numberOfTags)
 	{
-		while (allTastesAndTags.size() < numberOfTags) {
-			//Obtain a random tag from tags
+		while (allTastesAndTags.size() < numberOfTags)
+		{
+			// Obtain a random tag from tags
 			String randomTag = tags.values()[randomNumber.nextInt(tags.values().length)].toString();
-			if (!(allTastesAndTags.contains(randomTag))) {
+			if (!(allTastesAndTags.contains(randomTag)))
+			{
 				allTastesAndTags.add(randomTag);
 			}
 		}
 	}
-	
+
 	/**
 	 * Performs one simulation cycle which calls
 	 * a user to "act"
 	 * 
 	 * @author Raymond Wu
 	 */
-	public void simulationStep() 
+	public void simulationStep()
 	{
-		//Generate a random number so the simulation can get a random user
+		// Generate a random number so the simulation can get a random user
 		User randomUser = network.getUsers().get(randomNumber.nextInt(network.getUsers().size()));
-		
-		//Ask the random user to "act"
+
+		// Ask the random user to "act"
 		System.out.println("User: " + randomUser.getUserName() + " is acting!");
 		randomUser.act(network);
 	}
-	
+
 	/**
 	 * Simple-console based implementation of the
 	 * simulation
 	 * 
 	 * @author Raymond Wu
 	 */
-	public void simulationRun() 
+	public void simulationRun()
 	{
-		 	int numberOfTags;
-	        int numberOfProducers;
-	        int numberOfConsumers;
-	        String start;
-	        Scanner in = new Scanner(System.in);
-	        
-	        System.out.println("Welcome to a simulation of a file-sharing social network");
-	      /*
-	        System.out.println("Enter the number of tags: (1-6)");
-	        numberOfTags = in.nextInt();
-	        System.out.println("Enter the number of consumers:");
-	        numberOfConsumers = in.nextInt();
-	        System.out.println("Enter the number of producers:");
-	        numberOfProducers = in.nextInt();
-	        */
-	        numberOfTags = 2;
-	        numberOfConsumers = 2;
-	        numberOfProducers = 2;
-	        
-	        
-	        System.out.println("Would you like to run the simulation? (yes/no)");
-	        start = in.next();
-	 
-	        if (start.equals("yes")){
-	        	System.out.println("Simulation starting:");
-	          	setTastesAndTags(numberOfTags);
-	        	createProducers(numberOfProducers);
-	        	createConsumers(numberOfConsumers);
-	        
-	        	while (start.equals("yes")) {
-	        		
-	        		simulationStep();	
-	        		
-	        		System.out.println("Current users in the simulation:");
-	        		for (User user : network.getUsers()) {
-	        			System.out.print(user.toString() + "\n");
-	        		}
-	        		
-	        		System.out.println("Current documents in the simulation:");
-	        		for (Document document : network.getAllDocuments()) {
-	        			System.out.print(document.toString() + "\n");
-	        		}
-	        		
-	        		System.out.println("Would you like to continue the simulation? (yes/no)");
-	        		start = in.next();
-	        	}  
-	        }
-	        System.out.println("Exiting...");       
+		int numberOfTags;
+		int numberOfProducers;
+		int numberOfConsumers;
+		String start;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("Welcome to a simulation of a file-sharing social network");
+		/*
+		 * System.out.println("Enter the number of tags: (1-6)");
+		 * numberOfTags = in.nextInt();
+		 * System.out.println("Enter the number of consumers:");
+		 * numberOfConsumers = in.nextInt();
+		 * System.out.println("Enter the number of producers:");
+		 * numberOfProducers = in.nextInt();
+		 */
+		numberOfTags = 2;
+		numberOfConsumers = 2;
+		numberOfProducers = 2;
+
+		System.out.println("Would you like to run the simulation? (yes/no)");
+		start = in.next();
+
+		if (start.equals("yes"))
+		{
+			System.out.println("Simulation starting:");
+			setTastesAndTags(numberOfTags);
+			createProducers(numberOfProducers);
+			createConsumers(numberOfConsumers);
+
+			while (true)
+			{
+				// Break if user enters no
+				if (start.equalsIgnoreCase("no") || start.equalsIgnoreCase("stop"))
+					break;
+
+				switch (start.toLowerCase())
+				{
+
+				// Continue the simulation
+					case "go":
+						break;
+
+					// Print out all the stats
+					case "stats":
+					{
+						System.out.println("Current users in the simulation:");
+						for (User user : network.getUsers())
+						{
+							System.out.print(user.toString() + "\n");
+						}
+
+						System.out.println("Current documents in the simulation:");
+						for (Document document : network.getAllDocuments())
+						{
+							System.out.print(document.toString() + "\n");
+						}
+					}
+
+					// Any other input results in asking the user again
+					default:
+						System.out.println("Please Enter a Command (GO, STOP, STATS)");
+						start = in.next();
+
+						continue;
+
+				}
+
+				simulationStep();
+
+				System.out.println("Please Enter a Command (GO, STOP, STATS):");
+				start = in.next();
+
+				System.out.println();
+
+			}
+		}
+		System.out.println("Exiting...");
 	}
-		
-	public static void main(String[] args){
-			
-			Network network = new Network();
-			Simulator simulator = new Simulator(network);
-			simulator.simulationRun();
+
+	public static void main(String[] args)
+	{
+
+		Network network = new Network();
+		Simulator simulator = new Simulator(network);
+		simulator.simulationRun();
 	}
-	
+
 }
