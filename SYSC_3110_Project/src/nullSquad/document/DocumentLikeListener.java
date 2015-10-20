cbdc518f25148e220ae076974dae9379e3281ae7
MikeVezina <vezina95@bell.net>
2015-10-19 22:21:54 -0400

    Added DocumentLikeListener Interface
    Provides an interface which allows classes to implement a function that
    will get called when a document is liked

---
+++ SYSC_3110_Project/src/nullSquad/document/DocumentLikeListener.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package nullSquad.document;
+/**
+ * Document Like Listener Interface
+ * Interface for classes that would like to know when a document is liked
+ * 
+ * @author MVezina
+ */
+public interface DocumentLikeListener
+{
+	public void DocumentLiked(DocumentLikeEvent docLikeEvent);
+}
