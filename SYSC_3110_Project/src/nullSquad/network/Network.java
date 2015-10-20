eba8696c2d453f4a9dd8ea4606e9bbc0a90dbd2f
MikeVezina <vezina95@bell.net>
2015-10-20 10:37:23 -0400

    Fixed Network Class
    Initial userID will now be 1

--- SYSC_3110_Project/src/nullSquad/network/Network.java
+++ SYSC_3110_Project/src/nullSquad/network/Network.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class Network
 {
-	private int nextAvailableUserID = 0;
+	private int nextAvailableUserID = 1;
 	private List<User> users;
 	private List<Document> allDocuments;
 	
