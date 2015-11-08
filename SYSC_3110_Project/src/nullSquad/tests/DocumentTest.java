54a597bf252d9d4e69671454553f5af53d3d1512
marctebo <marctebo@gmail.com>
2015-11-08 16:10:46 -0500

    Updated DocumentTest Class
    Added empty test functions to class

--- SYSC_3110_Project/src/nullSquad/tests/DocumentTest.java
+++ SYSC_3110_Project/src/nullSquad/tests/DocumentTest.java
@@ -1,14 +1,93 @@
 package nullSquad.tests;
-import nullSquad.document.*;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DocumentTest {
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
-	public void test() {
+	public void testDocument() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddLikeListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveLikeListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLikeDocument() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnlikeDocument() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserLikes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTag() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDocumentID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateUploaded() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDocumentName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProducer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDocumentID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
 		fail("Not yet implemented");
 	}
 
