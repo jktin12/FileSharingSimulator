57ac80837319953d6959d302ff3bd518856da87f
Raymond Wu <Raymondwu@cmail.carleton.ca>
2015-11-18 16:30:11 -0500

    Added SimulatorTest

---
+++ SYSC_3110_Project/src/nullSquad/tests/SimulatorTest.java
@@ -0,0 +1,59 @@
+package nullSquad.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimulatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNetwork() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSimulator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSimulationStep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateConsumers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateProducers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentSimulatorSequence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCurrentSimulatorSequence() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTotalSimulatorSequences() {
+		fail("Not yet implemented");
+	}
+
+}
