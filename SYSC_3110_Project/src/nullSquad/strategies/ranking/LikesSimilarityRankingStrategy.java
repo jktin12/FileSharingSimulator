3545721a7d9f3923b377c2a609e6d08503df97b0
MikeVezina <vezina95@bell.net>
2015-11-19 23:17:34 -0500

    Added Empty Strategy Files for Future Implementation

---
+++ SYSC_3110_Project/src/nullSquad/strategies/ranking/LikesSimilarityRankingStrategy.java
@@ -0,0 +1,15 @@
+package nullSquad.strategies.ranking;
+
+import nullSquad.filesharingsystem.document.Document;
+
+public class LikesSimilarityRankingStrategy implements SearchRankingStrategy
+{
+
+	@Override
+	public int compare(Document o1, Document o2)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ SYSC_3110_Project/src/nullSquad/strategies/ranking/UserDistanceRankingStrategy.java
@@ -0,0 +1,15 @@
+package nullSquad.strategies.ranking;
+
+import nullSquad.filesharingsystem.document.Document;
+
+public class UserDistanceRankingStrategy implements SearchRankingStrategy
+{
+
+	@Override
+	public int compare(Document o1, Document o2)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
