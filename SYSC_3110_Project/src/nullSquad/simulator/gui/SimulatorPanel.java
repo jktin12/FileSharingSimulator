3546b4350bcd9b282de8fb2babed2f8502580c2a
MikeVezina <vezina95@bell.net>
2015-11-19 23:18:12 -0500

    Updated Simulator Tab Panel

--- SYSC_3110_Project/src/nullSquad/simulator/gui/SimulatorPanel.java
+++ SYSC_3110_Project/src/nullSquad/simulator/gui/SimulatorPanel.java
@@ -1,18 +1,22 @@
 package nullSquad.simulator.gui;
 
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 /**
  * Representation of the Simulator Tab Panel
+ * 
  * @author MVezina
  */
-public class SimulatorPanel extends JPanel {
-
+public class SimulatorPanel extends JPanel
+{
 
 	private JTextArea mainSimulatorTextArea;
-	
+
 	/**
 	 * Creates the simulator panel and all associated components
+	 * 
 	 * @author MVezina
 	 */
 	public SimulatorPanel()
@@ -22,13 +26,18 @@ public class SimulatorPanel extends JPanel {
 		// Create a scrollable JTextArea pane
 		mainSimulatorTextArea = new JTextArea("Welcome to the Simulator!\n");
 		mainSimulatorTextArea.setEditable(false);
+
 		this.add(new JScrollPane(mainSimulatorTextArea));
 	}
 
-	public void setLogText(String logText) {
+	/**
+	 * Sets the Log Text Area Text
+	 * 
+	 * @param logText
+	 */
+	public void setLogText(String logText)
+	{
 		mainSimulatorTextArea.setText(logText);
 	}
-	
-	
-	
+
 }
