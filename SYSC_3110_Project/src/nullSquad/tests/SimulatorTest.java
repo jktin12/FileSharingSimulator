564d6b547257d060c2b2ec3b5a7e165961344a53
Raymond Wu <raymondwu@cmail.carleton.ca>
2015-11-20 12:39:54 -0500

    Implemented SimulatorTest
    -Added all the test cases

--- SYSC_3110_Project/src/nullSquad/tests/SimulatorTest.java
+++ SYSC_3110_Project/src/nullSquad/tests/SimulatorTest.java
@@ -1,15 +1,54 @@
+/**
+ * Title: SYSC 3110 Project
+ * Created By: Raymond Wu
+ * Student Number: 100938326
+ * Team: nullSquad
+ */
+
 package nullSquad.tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import nullSquad.filesharingsystem.FileSharingSystem;
+import nullSquad.filesharingsystem.users.Consumer;
+import nullSquad.filesharingsystem.users.Producer;
+import nullSquad.simulator.Simulator;
+
 public class SimulatorTest {
 
+	private FileSharingSystem network;
+	private Simulator simulator;
+    private String programmingTag, bookTag, musicTag, sportsTag;
+    private List<String> tags ;
+	private int currentSimulatorSequenceTest;
+	private int totalSimulatorSequencesTest;
+	
+	
 	@Before
 	public void setUp() throws Exception {
+			programmingTag = "Programming";
+	        bookTag = "Book";
+	        musicTag = "Music";
+	        sportsTag = "Sports";
+	     
+	        tags = new ArrayList<>();
+			tags.add(programmingTag);
+			tags.add(bookTag);
+			tags.add(musicTag);
+			tags.add(sportsTag);
+			
+			totalSimulatorSequencesTest=10;
+			currentSimulatorSequenceTest=0;
+			
+			network = new FileSharingSystem(tags);
+			simulator = new Simulator(network,totalSimulatorSequencesTest);
 	}
 
 	@After
@@ -18,42 +57,63 @@ public class SimulatorTest {
 
 	@Test
 	public void testGetNetwork() {
-		fail("Not yet implemented");
+		assertEquals(simulator.getNetwork(),network);
 	}
 
 	@Test
 	public void testSimulator() {
-		fail("Not yet implemented");
+		assertEquals(simulator.getTotalSimulatorSequences(),totalSimulatorSequencesTest);
+		assertEquals(simulator.getCurrentSimulatorSequence(),currentSimulatorSequenceTest);
+		assertEquals(simulator.getNetwork(),network);
 	}
 
 	@Test
-	public void testSimulationStep() {
-		fail("Not yet implemented");
+	public void testCreateConsumersEmpty() {
+		simulator.createConsumers(0);
+		assertTrue(network.getUsers().isEmpty());
 	}
-
+	
 	@Test
-	public void testCreateConsumers() {
-		fail("Not yet implemented");
+	public void testCreateConsumers(){
+		simulator.createConsumers(2);
+		assertEquals(network.getUsers().size(),2);
+		assertTrue(network.getUsers().get(0)instanceof Consumer);
+		assertTrue(network.getUsers().get(1)instanceof Consumer);
+		network.deactivateUser(network.getUsers().get(1));
+		network.deactivateUser(network.getUsers().get(0));
+	}
+	
+	@Test
+	public void testCreateProducersEmpty(){
+		simulator.createProducers(0);
+		assertTrue(network.getUsers().isEmpty());
 	}
 
 	@Test
 	public void testCreateProducers() {
-		fail("Not yet implemented");
+		simulator.createProducers(2);
+		assertEquals(network.getUsers().size(),2);
+		assertTrue(network.getUsers().get(0)instanceof Producer);
+		assertTrue(network.getUsers().get(0)instanceof Producer);
+		network.deactivateUser(network.getUsers().get(1));
+		network.deactivateUser(network.getUsers().get(0));
 	}
 
 	@Test
 	public void testGetCurrentSimulatorSequence() {
-		fail("Not yet implemented");
+		assertEquals(simulator.getCurrentSimulatorSequence(),currentSimulatorSequenceTest);
 	}
 
 	@Test
 	public void testSetCurrentSimulatorSequence() {
-		fail("Not yet implemented");
+		simulator.setCurrentSimulatorSequence(2);
+		currentSimulatorSequenceTest=2;
+		assertEquals(simulator.getCurrentSimulatorSequence(),currentSimulatorSequenceTest);
 	}
 
 	@Test
 	public void testGetTotalSimulatorSequences() {
-		fail("Not yet implemented");
+		assertEquals(simulator.getTotalSimulatorSequences(),totalSimulatorSequencesTest);
 	}
 
 }
