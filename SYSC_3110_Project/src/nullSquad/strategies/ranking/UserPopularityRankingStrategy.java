5da27a41d108e4b3fef1db6b5c0ee9f26845e37a
MikeVezina <vezina95@bell.net>
2015-11-20 22:36:17 -0500

    Updated Project Settings

--- SYSC_3110_Project/src/nullSquad/strategies/ranking/UserPopularityRankingStrategy.java
+++ SYSC_3110_Project/src/nullSquad/strategies/ranking/UserPopularityRankingStrategy.java
@@ -1,17 +1,58 @@
 package nullSquad.strategies.ranking;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import nullSquad.filesharingsystem.document.Document;
 import nullSquad.filesharingsystem.users.User;
 
-public class UserPopularityRankingStrategy implements DocumentRankingStrategy
+public class UserPopularityRankingStrategy implements DocumentRankingStrategy, Comparator<Document>
 {
 
 	@Override
 	public List<Document> rankDocuments(List<Document> allDocuments, User user, String tag, int topK)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		List<Document> rankedDocuments = new ArrayList<>();
+
+		for (Document d : allDocuments)
+		{
+			if (d.getTag().equals(tag))
+			{
+				rankedDocuments.add(d);
+			}
+		}
+
+		// Go through all user likes and rank based on
+
+		return rankedDocuments;
+
 	}
+
+	@Override
+	public int compare(Document document1, Document document2)
+	{
+		int avgUserPopularityDocument1 = 0, avgUserPopularityDocument2 = 0;
+
+		// Determine the overall popularity of all likers of document 1
+		for (User u : document1.getUserLikes())
+		{
+			avgUserPopularityDocument1 += u.getFollowers().size();
+		}
+
+		avgUserPopularityDocument1 /= document1.getUserLikes().size();
+
+		// Determine the overall popularity of all likers of document 1
+		for (User u : document2.getUserLikes())
+		{
+			avgUserPopularityDocument2 += u.getFollowers().size();
+		}
+
+		avgUserPopularityDocument2 /= document2.getUserLikes().size();
+
+		
+		// Return the difference of the two user popularity averages
+		return avgUserPopularityDocument1 - avgUserPopularityDocument2;
+	}
+
 }
