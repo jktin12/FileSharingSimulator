cc1b33612c3140454e41ee5baeb3cf485d5919f6
Justin Krol <justinkrol11@gmail.com>
2015-11-22 13:29:36 -0500

    Implemented UserDistanceRankingStrategy and added test case
    Implemented using two different approaches:
    1: checks friends and then friends of friends for likers of the
    document (iterative)
    2: checks for distance in network in terms of layers of friends before
    a liker of the document is found (recursive)

--- SYSC_3110_Project/src/nullSquad/strategies/ranking/UserDistanceRankingStrategy.java
+++ SYSC_3110_Project/src/nullSquad/strategies/ranking/UserDistanceRankingStrategy.java
@@ -1,16 +1,164 @@
 package nullSquad.strategies.ranking;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import nullSquad.filesharingsystem.document.Document;
 import nullSquad.filesharingsystem.users.User;
 
-public class UserDistanceRankingStrategy implements DocumentRankingStrategy
+public class UserDistanceRankingStrategy implements DocumentRankingStrategy, Comparator<Document>
 {
+	private User user;
+	private final int MAX_DEPTH_TO_SEARCH = 4;
+
 	@Override
 	public List<Document> rankDocuments(List<Document> allDocuments, User user)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		// Set global User variable to the User passed in.
+		this.user = user;
+
+		// Copy the documents over to the new array to be ranked
+		List<Document> rankedDocuments = new ArrayList<>();
+		for (Document d : allDocuments)
+		{
+			rankedDocuments.add(d);
+		}
+
+		// Sort the top documents using the selected strategy as a comparator
+		Collections.sort(rankedDocuments, this);
+
+		// Since Collections.sort() sorts from worst -> greatest, we need to
+		// reverse the list
+		Collections.reverse(rankedDocuments);
+
+		// Return the ranked documents
+		return rankedDocuments;
 	}
+
+	public int compare(Document doc1, Document doc2)
+	{
+		// If both are null, they are ranked the same
+		if (doc1 == null && doc2 == null)
+			return 0;
+
+		// If doc1 is null and doc2 isnt, doc2 is ranked higher
+		if (doc1 == null && doc2 != null)
+			return -1;
+
+		// If doc1 isnt null and doc2 is, doc1 is ranked higher
+		if (doc1 != null && doc2 == null)
+			return 1;
+
+		// Compare documents in terms of immediacy of friends
+		// check if the document is liked by an immediate friend.
+		// if not, check if any friends of friends like the document.
+		int doc1_val = 5, doc2_val = 5;
+
+		//doc1_val = getFriendDistance();
+		
+		if (doc1_val < doc2_val)
+		{
+			return 1;
+		}
+		if (doc1_val > doc2_val)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+		// can also try recursive or iterative breadth-first-search
+		// need to have a way to mark Users as "visited"
+		// how to implement this?
+
+	}
+
+	/*
+	 * Only checks friends and friends of friends.
+	 */
+	private int getFriendDistance(User user, Document doc){
+		int depth = 3;
+		if (docLikedByFriend(user, doc))
+		{
+			depth = 1;
+		}
+		else
+		{
+			for (User u : getFriendsList(user))
+			{
+				if (docLikedByFriend(u, doc))
+				{
+					depth = 2;
+					break;
+				}
+			}
+		}
+		return depth;
+	}
+	
+	/*
+	 * Recursive solution.
+	 */
+	private int getFriendDistance(User user, Document doc, int depth){
+		int minDepth = MAX_DEPTH_TO_SEARCH;		//i.e. starts off greater than all other possible depths
+		int tempDepth = 0;						//reset with every recursive call at a specific depth
+		
+		//Base Case: One of this user's friends likes the document
+		if (docLikedByFriend(user, doc))
+		{
+			return 1;		//return 1 to represent a distance of one friend
+		}
+		
+		//If none of this user's friends like the document.
+		else{
+			if(depth < MAX_DEPTH_TO_SEARCH){		//if we are at less than the maximum allowable depth
+				for(User u: getFriendsList(user)){	//for every user in this user's friend list		
+					tempDepth = getFriendDistance(u, doc, depth+1); //get the distance to a liker of the document
+																	//pass depth+1 to indicate another layer of depth,
+																	//so that the recursion does not continue forever.
+					if(tempDepth < minDepth){		//if the depth for this user is less than the previous minimum,	
+						minDepth = tempDepth;		//set it as the new minimum.
+					}
+				}
+				return minDepth + 1;	// return the minimum depth obtained
+										// +1 to factor in the current level of depth.
+			}
+			else{
+				return 2;
+			}
+		}
+	}
+
+	/**
+	 * Merges Followers/Following to form friends list
+	 * 
+	 * @param user The User's List of Friends to get
+	 * @return The User's List of Friends
+	 * @author MVezina
+	 */
+	private List<User> getFriendsList(User user)
+	{
+		// Merge followers and following to form friends list
+		List<User> friends = new ArrayList<>(user.getFollowers());
+		friends.addAll(user.getFollowing());
+
+		return friends;
+	}
+
+	private boolean docLikedByFriend(User userTemp, Document doc)
+	{
+		for (User u : getFriendsList(userTemp))
+		{
+			if (u.getLikedDocuments().contains(doc))
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 }
