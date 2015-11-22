26bf2ab1584182d1e813eba29974f081aa2c05ac
marctebo <marctebo@gmail.com>
2015-11-22 12:16:14 -0500

    Added Test Case for Follow Similarity
    added FollowSimilarityStrategyTest.java...need to complete methods

---
+++ SYSC_3110_Project/src/nullSquad/strategies/ranking/FollowSimilarityStrategyTest.java
@@ -0,0 +1,29 @@
+package nullSquad.strategies.ranking;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FollowSimilarityStrategyTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRankDocuments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompare() {
+		fail("Not yet implemented");
+	}
+
+}
